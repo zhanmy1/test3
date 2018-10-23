4e00428b566efec8ef5cca5a7c0274b747580c27
zhanmy1 <382634619@qq.com>
2018-10-23 20:08:08 +0800

    check_card

---
+++ if__if.py
@@ -0,0 +1,10 @@
+ticket = input("你是否有车票？")
+if ticket=="有":
+	print("通过车票的检查，进入安检")
+	knifeLenght = int(input("你携带的刀具的长度："))
+	if knifeLenght<=10:
+		print("通过")
+	else:
+		print("请留下你的刀")
+else:
+	print("请到售票厅买票")
